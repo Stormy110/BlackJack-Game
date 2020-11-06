5637cd347a148d7b22bc39baef953bb117e3931e
Ian Storms <IanStorms110@yahoo.com>
2020-11-06 16:05:09 -0500

    images

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+let button = document.querySelector('#deal-button')
+let playerHand = document.querySelector('#player-hand')
+let dealerHand = document.querySelector('#dealer-hand')
+
+const funct1 = function(evt){
+  console.log('hello')
+  let card = document.createElement('img')
+}
+button.onclick = funct1
+  
+
+
 
 window.addEventListener('DOMContentLoaded', function() {
-    // Execute after page load
-  })
+//Execute after page//
+})
